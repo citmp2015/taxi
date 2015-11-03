f501c0f8451c915423bf30437b174ddbd591fd5e
fabsi110 <f.schuerer@campus.tu-berlin.de>
2015-11-03 23:46:01 +0100

    Made PaymentType_Tip the new main class.

--- src/main/java/org/tuberlin/de/paymenttype_tip/PaymentType_Tip.java
+++ src/main/java/org/tuberlin/de/paymenttype_tip/PaymentType_Tip.java
@@ -3,15 +3,10 @@ package org.tuberlin.de.paymenttype_tip;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.FlatMapOperator;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 import org.tuberlin.de.geodata.MapCoordToDistrict;
 import org.tuberlin.de.read_data.Taxidrive;
-
-import java.util.DoubleSummaryStatistics;
-
 /**
  * Created by Fabian on 24.10.2015.
  */
