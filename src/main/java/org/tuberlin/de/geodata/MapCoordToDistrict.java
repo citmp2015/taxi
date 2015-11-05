a913dea9901026aac32d78c9d62546fac4706d0f
gerrit <gerrit-janssen@t-online.de>
2015-11-05 10:52:34 +0100

    -- district geometries are now read from a common textfile -- moved shapefilecode to its own class -- rearranged code

--- src/main/java/org/tuberlin/de/geodata/MapCoordToDistrict.java
+++ src/main/java/org/tuberlin/de/geodata/MapCoordToDistrict.java
@@ -1,26 +1,13 @@
 package org.tuberlin.de.geodata;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.configuration.Configuration;
-
-import org.geotools.data.DataStore;
-import org.geotools.data.DataStoreFinder;
-import org.geotools.data.FeatureSource;
-import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureIterator;
-
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
 import org.tuberlin.de.read_data.Taxidrive;
 
-import java.io.*;
-import java.net.MalformedURLException;
-import java.util.*;
+import java.util.Collection;
 
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -42,207 +29,150 @@ import java.util.*;
 
 /**
  * maps longitude, latitude coordinates from taxi trips to the appropriate district of manhattan.
- *
  */
 
 public class MapCoordToDistrict {
 
-	public static void main(String[] args) throws Exception {
-		// set up the execution environment
-		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+    public static void main(String[] args) throws Exception {
+        // set up the execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		// load taxi data from csv-file
-		DataSet<Taxidrive> taxidrives = env.readCsvFile("data/testData.csv")
-				.pojoType(Taxidrive.class,
-						"taxiID",
-						"licenseID",
-						"pickup_datetime",
-						"dropoff_datetime",
-						"trip_time_in_secs",
-						"trip_distance",
-						"pickup_longitude",
-						"pickup_latitude",
-						"dropoff_longitude",
-						"dropoff_latitude",
-						"payment_type",
-						"fare_amount",
-						"surcharge",
-						"mta_tax",
-						"tip_amount",
-						"tolls_amount",
-						"total_amount");
+        //ShapeToTextConverter.convertShape("data/manhattan_districts.shp", "data/districts");
 
-		//load districts
-		//Collection<District> col = extractDistrictsFromShapefile("data/manhattan_districts.shp");
+        // load taxi data from csv-file and map districts
+        DataSet<Taxidrive> taxidrives = readData(env, "data/testData.csv");
 
-		//DataSet<District> districtGeometries = env.fromCollection(col); //env.readCsvFile("data/districtsAsText").pojoType(District.class, "district","geometry");
-		DataSet<String> districtGeometriesAsText = env.readTextFile("data/districtsAsText");
-
-		DataSet<District> districtGeometries = districtGeometriesAsText.map(new MapFunction<String, District>() {
-			@Override
-			public District map(String s) throws Exception {
-				String sep[] = s.split(",");
-				District d = new District();
-				d.district = sep[0];
-
-				int countOfCoords = (sep.length - 1) / 2;
-				org.tuberlin.de.geodata.Coordinate[] geometry = new org.tuberlin.de.geodata.Coordinate[countOfCoords];
-				int geometryIndex = 0;
-
-				for (int i = 1; i < sep.length; i += 2) {
-					org.tuberlin.de.geodata.Coordinate c = new org.tuberlin.de.geodata.Coordinate();
-					c.x = Double.parseDouble(sep[i]);
-					c.y = Double.parseDouble(sep[i + 1]);
-					geometry[geometryIndex] = c;
-					geometryIndex += 1;
-				}
-				d.geometry = geometry;
-				System.out.println(d.toString());
-				return d;
-			}
-		});
-
-		//env.fromCollection(col);
-
-		//districtGeometries.writeAsText("data/districtsAsText");
-		//districtGeometries.print();
-		// map pickup/dropoff-coordinates to districts
-		// district dataset is used as broadcast variable (https://cwiki.apache.org/confluence/display/FLINK/Variables+Closures+vs.+Broadcast+Variables)
-		taxidrives = taxidrives.map(new DistrictMapper())
-				.withBroadcastSet(districtGeometries, "districtGeometries");
-
-		//taxidrives.writeAsText("data/testDataWithDistricts");
-		taxidrives.print();
-		// execute program
-		//env.execute("Flink Java API Skeleton");
-	}
+        //taxidrives.writeAsText("data/testDataWithDistricts");
+        taxidrives.print();
 
 
-	/**
-	 *
-	 * @param path to shapefile (extension .shp)
-	 * @return a collection of districts containing geometries and district names
-	 */
+        // execute program
+        //env.execute("Flink Java API Skeleton");
+    }
 
-	public static Collection<District> extractDistrictsFromShapefile(String path) {
-		File file = new File(path);
-		Map<String, Object> map = new HashMap<String, Object>();
-		FeatureSource<SimpleFeatureType, SimpleFeature> source = null;
-		DataStore dataStore = null;
-		FeatureIterator<SimpleFeature> features = null;
-		try {
+    public static DataSet<Taxidrive> readData(ExecutionEnvironment env, String dataPath) throws Exception {
+        // set up the execution environment
+        // load taxi data from csv-file
+        DataSet<Taxidrive> taxidrives = env.readCsvFile(dataPath)
+                .pojoType(Taxidrive.class,
+                        "taxiID",
+                        "licenseID",
+                        "pickup_datetime",
+                        "dropoff_datetime",
+                        "trip_time_in_secs",
+                        "trip_distance",
+                        "pickup_longitude",
+                        "pickup_latitude",
+                        "dropoff_longitude",
+                        "dropoff_latitude",
+                        "payment_type",
+                        "fare_amount",
+                        "surcharge",
+                        "mta_tax",
+                        "tip_amount",
+                        "tolls_amount",
+                        "total_amount");
 
-			map.put("url", file.toURI().toURL());
+        //load districts
+        DataSet<String> districtGeometriesAsText = env.readTextFile("data/districts");
 
-			dataStore = DataStoreFinder.getDataStore(map);
+        DataSet<District> districtGeometries = districtGeometriesAsText.map(new MapFunction<String, District>() {
+            @Override
+            public District map(String s) throws Exception {
+                String sep[] = s.split(",");
+                District d = new District();
+                d.district = sep[0];
 
-			String typeName = dataStore.getTypeNames()[0];
+                int countOfCoords = (sep.length - 1) / 2;
+                org.tuberlin.de.geodata.Coordinate[] geometry = new org.tuberlin.de.geodata.Coordinate[countOfCoords];
+                int geometryIndex = 0;
 
-			source = dataStore
-					.getFeatureSource(typeName);
+                for (int i = 1; i < sep.length; i += 2) {
+                    org.tuberlin.de.geodata.Coordinate c = new org.tuberlin.de.geodata.Coordinate();
+                    c.x = Double.parseDouble(sep[i]);
+                    c.y = Double.parseDouble(sep[i + 1]);
+                    geometry[geometryIndex] = c;
+                    geometryIndex += 1;
+                }
+                d.geometry = geometry;
+                System.out.println(d.toString());
+                return d;
+            }
+        });
+        // map pickup/dropoff-coordinates to districts
+        // district dataset is used as broadcast variable (https://cwiki.apache.org/confluence/display/FLINK/Variables+Closures+vs.+Broadcast+Variables)
+        taxidrives = taxidrives.map(new DistrictMapper())
+                .withBroadcastSet(districtGeometries, "districtGeometries");
 
-
-			FeatureCollection<SimpleFeatureType, SimpleFeature> collection = null;
-			collection = source.getFeatures();
+        return taxidrives;
+    }
 
 
 
-		HashSet<District> feats = new HashSet<District>();
-		features = collection.features();
-			while (features.hasNext()) {
-				SimpleFeature feature = features.next();
-				District d =  new District();
-				d.district = (String) feature.getAttribute("label");
-				d.geometry = convertGeometry(((Geometry) feature.getDefaultGeometry()).getCoordinates());
-				feats.add(d);
-			}
-			return feats;
-
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			features.close();
-			dataStore.dispose();
-		}
 
 
-		return null;
-	}
+    public static boolean coordinateInRegion(org.tuberlin.de.geodata.Coordinate[] region, Coordinate coord) {
+        // adapt the code from http://stackoverflow.com/questions/12083093/how-to-define-if-a-determinate-point-is-inside-a-region-lat-long
+        int i, j;
+        boolean isInside = false;
+        //create an array of coordinates from the region boundary list
+        int sides = region.length;
+        for (i = 0, j = sides - 1; i < sides; j = i++) {
+            //verifying if your coordinate is inside your region
+            if (
+                    (
+                            (
+                                    (region[i].x <= coord.x) && (coord.x < region[j].x)
+                            ) || (
+                                    (region[j].x) <= coord.x) && (coord.x < region[i].x)
+                    )
+                            &&
+                            (coord.y < (region[j].y - region[i].y) * (coord.y - region[i].x) / (region[j].x - region[i].x) + region[i].y)
+                    ) {
+                isInside = !isInside;
+            }
+        }
+        return isInside;
+    }
 
-	private static org.tuberlin.de.geodata.Coordinate[] convertGeometry(Coordinate[] coordinates) {
-		org.tuberlin.de.geodata.Coordinate[] converted = new org.tuberlin.de.geodata.Coordinate[coordinates.length];
+    public static class DistrictMapper extends RichMapFunction<Taxidrive, Taxidrive> {
 
-		for (int i = 0; i < coordinates.length; i++) {
-			Coordinate c = coordinates[i];
-			org.tuberlin.de.geodata.Coordinate conv = new org.tuberlin.de.geodata.Coordinate();
-			conv.x = c.x;
-			conv.y = c.y;
-			converted[i] = conv;
-		}
+        private Collection<District> districtGeometries;
 
-		return converted;
-	}
-
-
-	public static class DistrictMapper extends RichMapFunction<Taxidrive, Taxidrive> {
-
-		private Collection<District> districtGeometries;
-
-		@Override
-		public void open(Configuration parameters) {
-			this.districtGeometries = getRuntimeContext().getBroadcastVariable("districtGeometries");
-		}
-
-
-		@Override
-		public Taxidrive map(Taxidrive taxidrive) throws Exception {
-
-			for(District feat : districtGeometries) {
-
-				// pickup location
-				if (coordinateInRegion(feat.geometry, new Coordinate(taxidrive.dropoff_longitude, taxidrive.dropoff_latitude))) {
-					taxidrive.setPickupDistrict(feat.district);
-					break;
-				}
-			}
-
-			for(District feat : districtGeometries) {
-				// dropoff location
-				if(coordinateInRegion(feat.geometry, new Coordinate(taxidrive.dropoff_longitude, taxidrive.dropoff_latitude))){
-					taxidrive.setDropoffDistrict(feat.district);
-					break;
-				}
-			}
-			return taxidrive;
-		}
-	}
+        @Override
+        public void open(Configuration parameters) {
+            this.districtGeometries = getRuntimeContext().getBroadcastVariable("districtGeometries");
+        }
 
 
-	public static boolean coordinateInRegion(org.tuberlin.de.geodata.Coordinate[] region, Coordinate coord) {
-		// adapt the code from http://stackoverflow.com/questions/12083093/how-to-define-if-a-determinate-point-is-inside-a-region-lat-long
-		int i, j;
-		boolean isInside = false;
-		//create an array of coordinates from the region boundary list
-		int sides = region.length;
-		for (i = 0, j = sides - 1; i < sides; j = i++) {
-			//verifying if your coordinate is inside your region
-			if (
-					(
-							(
-									(region[i].x <= coord.x) && (coord.x < region[j].x)
-							) || (
-									(region[j].x) <= coord.x) && (coord.x < region[i].x)
-							)
-					 &&
-							(coord.y < (region[j].y - region[i].y) * (coord.y - region[i].x) / (region[j].x - region[i].x) + region[i].y)
-					) {
-				isInside = !isInside;
-			}
-		}
-		return isInside;
-	}
+        @Override
+        public Taxidrive map(Taxidrive taxidrive) throws Exception {
+
+            for (District feat : districtGeometries) {
+                Coordinate c = new Coordinate();
+                c.x = taxidrive.dropoff_longitude;
+                c.y = taxidrive.dropoff_latitude;
+                // pickup location
+                if (coordinateInRegion(feat.geometry, c)) {
+                    taxidrive.setPickupDistrict(feat.district);
+                    break;
+                }
+            }
+
+            for (District feat : districtGeometries) {
+                // dropoff location
+                Coordinate c = new Coordinate();
+                c.x = taxidrive.dropoff_longitude;
+                c.y = taxidrive.dropoff_latitude;
+
+                if (coordinateInRegion(feat.geometry, c)) {
+                    taxidrive.setDropoffDistrict(feat.district);
+                    break;
+                }
+            }
+            return taxidrive;
+        }
+    }
 }
 
 
