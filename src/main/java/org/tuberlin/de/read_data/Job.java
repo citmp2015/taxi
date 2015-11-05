47261316a1e4470b2b9ef4def5a6907f5617b00f
Iwailo Denisow <iwailodenisow@mail.tu-berlin.de>
2015-11-05 10:38:28 +0100

    Remove unnecessary comments

--- src/main/java/org/tuberlin/de/read_data/Job.java
+++ src/main/java/org/tuberlin/de/read_data/Job.java
@@ -1,45 +1,8 @@
 package org.tuberlin.de.read_data;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * Skeleton for a Flink Job.
- * <p/>
- * For a full example of a Flink Job, see the WordCountJob.java file in the
- * same package/directory or have a look at the website.
- * <p/>
- * You can also generate a .jar file that you can submit on your Flink
- * cluster.
- * Just type
- * mvn clean package
- * in the projects root directory.
- * You will find the jar in
- * target/flink-quickstart-0.1-SNAPSHOT-Sample.jar
- */
 public class Job {
     public static final class TaxidriveReader implements FlatMapFunction<String, Taxidrive> {
         @Override
